fc3bf93bfb3606266d14a7b12e3eacea6d7130de
ILPYX <1574862316@qq.com>
2020-02-07 11:06:32 +0800

    复习自定义表格

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -47,6 +47,26 @@
           <Content :style="{padding: '24px', minHeight: '280px', background: '#fff'}">
             Content
             <TimePicker type="time" placeholder="Select time" style="width: 168px"></TimePicker>
+            <table cellspacing="0" class="tb">
+              <thead>
+                <tr>
+                  <th>编号</th>
+                  <th>品牌名称</th>
+                  <th>创建时间</th>
+                  <th>操作</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="(item, index) in dataList">
+                  <td>{{index + 1}}</td>
+                  <td>{{item.name}}</td>
+                  <td>{{item.time}}</td>
+                  <td>
+                    <a href="#">删除</a>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </Content>
         </Layout>
       </Layout>
@@ -60,7 +80,13 @@ import Aside from "./common/Aside";
 
 export default {
   data() {
-    return {};
+    return {
+      dataList: [
+        { name: '华为', time: Date.now() },
+        { name: '小米', time: Date.now() },
+        { name: '魅族', time: Date.now() }
+      ]
+    };
   },
   methods: {},
   components: {
@@ -71,4 +97,24 @@ export default {
 </script>
 
 <style scoped>
+.tb {
+  border-collapse: collapse;
+  /* width: 100%; */
+  margin-top: 10px;
+}
+.tb th {
+  background-color: #0094ff;
+  color: white;
+}
+.tb td,
+.tb th {
+  padding: 5px;
+  border: 1px solid black;
+  text-align: center;
+}
+.add {
+  padding: 5px;
+  border: 1px solid black;
+  margin-bottom: 10px;
+}
 </style>
